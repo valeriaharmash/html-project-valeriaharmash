e28cbd141bcffd152488ed73cdd90299263b7c6b
Valeriia Harmash <valeriaharmash@gmail.com>
2023-07-24 18:01:25 -0400

    lint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const uniqeHome =  document.getElementById('uniqe-home');
+const landing = document.getElementById('landing')
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
+		})
+	})
+})
 
-uniqeHome.onclick = function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-  };
+landing.onclick = function () {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	})
+}
